7e0a636c37022dfcc51136f3d1fb13e529091342
shashank-jain2605 <shashank.jain2605@gmail.com>
2021-09-17 02:26:15 +0530

    Updates

---
+++ precache-manifest.0cec3aa0b355b3c60890a241fb3410c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8eb4c8bb027f652a6f991ce09a2bd0e2",
+    "url": "/shashank-jain2605/react-test.git/index.html"
+  },
+  {
+    "revision": "869b1c8b5035f35ef652",
+    "url": "/shashank-jain2605/react-test.git/static/css/main.f2f9e4d3.chunk.css"
+  },
+  {
+    "revision": "4646610705680ce7d806",
+    "url": "/shashank-jain2605/react-test.git/static/js/2.a99e8b18.chunk.js"
+  },
+  {
+    "revision": "849657740f66d00720db130cc1e94c97",
+    "url": "/shashank-jain2605/react-test.git/static/js/2.a99e8b18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "869b1c8b5035f35ef652",
+    "url": "/shashank-jain2605/react-test.git/static/js/main.9066cc08.chunk.js"
+  },
+  {
+    "revision": "5692a7a456282f998c2c",
+    "url": "/shashank-jain2605/react-test.git/static/js/runtime-main.8ed6d773.js"
+  }
+]);
